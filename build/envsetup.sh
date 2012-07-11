29f6e28d9d0742ba678e1040bd19e6b243f368ca
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-10 22:13:48 -0500

    envsetup: add breakfast
    Change-Id: I9ac8a4025d9ccfd5bee31a5fe9d1b490171bcbe7
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -16,3 +16,35 @@ function mka() {
             ;;
     esac
 }
+
+function breakfast()
+{
+    target=$1
+    VOLTAGE_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/voltage/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        echo "Nothing to eat for breakfast?"
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the voltage model name
+            lunch voltage_$target-userdebug
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
