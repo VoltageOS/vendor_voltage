8f3c206ef67f125fbe5b20aea6d114841153bdf6
xplodwild <xplod@ouverta.fr>
2013-08-24 17:40:37 +0100

    envsetup: add mka alias
    Change-Id: I4c057d3f59978dd5c20452b334d94c98ebc22659
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,5 +1,18 @@
 function __print_voltage_functions_help() {
 cat <<EOF
 Additional functions:
+- mka:             Builds using SCHED_BATCH on all processors.
 EOF
+}
+
+# Make using all available CPUs
+function mka() {
+    case `uname -s` in
+        Darwin)
+            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
+            ;;
+        *)
+            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            ;;
+    esac
 }
