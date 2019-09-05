a30727acdf4efdaa653ec6ce5d1719fe5b8accb1
Alessandro Astone <ales.astone@gmail.com>
2019-09-06 01:13:17 +0200

    vendor: soong: _contents -> _json_contents
    Change-Id: I2baa36bbb9aff3a7e70a293ed3352fb718eb611c
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,11 +1,11 @@
 add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 
-_contents := $(_contents)    "voltage":{$(newline)
+_json_contents := $(_json_contents)    "voltage":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
 
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
+_json_contents := $(_json_contents)__SV_END
 
-_contents := $(_contents)    },$(newline)
+_json_contents := $(_json_contents)    },$(newline)
