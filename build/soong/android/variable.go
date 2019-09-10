15a9bb3755d6c121d8d0408f1c3f04d47b1dd00f
Han Wang <416810799@qq.com>
2019-09-10 13:08:04 +0200

    soong_config: Add new flag for vendor_init
    Change-Id: I383f1de230ecf7b79d78fe701b140bd1bc6795f2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,10 +1,14 @@
 package android
 type Product_variables struct {
+	Target_init_vendor_lib struct {
+		Whole_static_libs []string
+	}
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
 }
 
 type ProductVariables struct {
+	Target_init_vendor_lib  *string `json:",omitempty"`
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,6 +3,7 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _json_contents := $(_json_contents)    "voltage":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
+$(call add_json_str_omitempty, Target_init_vendor_lib, $(TARGET_INIT_VENDOR_LIB))
 $(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
