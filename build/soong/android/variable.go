7e8ac67f9d4e20c8f0286cbfbd6fa5ad91e4e8c9
Michael Bestas <mkbestas@lineageos.org>
2019-09-20 01:15:25 +0300

    soong: Add AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT
    Change-Id: Iac6366d714ab3634def648e6a8c0c30d684a9383
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,13 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
+	Supports_extended_compress_format struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
 	Target_init_vendor_lib  *string `json:",omitempty"`
 	Additional_gralloc_10_usage_bits  *string `json:",omitempty"`
+	Supports_extended_compress_format  *bool `json:",omitempty"`
 }
