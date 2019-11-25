ce148488ddc3937a5f18cdc3e64f8afd8eb92d12
Kevin Haggerty <haggertk@lineageos.org>
2019-11-25 05:00:48 -0500

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,7 +3,7 @@ add_json_str_omitempty = $(if $(strip $(2)),$(call add_json_str, $(1), $(2)))
 _json_contents := $(_json_contents)    "voltage":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-
+$(call add_json_str_omitempty, Additional_gralloc_10_usage_bits, $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _json_contents := $(_json_contents)__SV_END
