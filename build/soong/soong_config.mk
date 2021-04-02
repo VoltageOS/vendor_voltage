yashsonawane/platform_vendor_lucid

vendor: add soong bits for boardflags

build/soong/soong_config.mk
@@ -1,0 +1,9 @@
+_contents := $(_contents)    "Lucid":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)

VoltageOS/vendor_voltage

Signed-off-by: Dmitrii <bankersenator@gmail.com>

build/soong/soong_config.mk
@@ -1,0 +1,9 @@
+_contents := $(_contents)    "voltage":{$(newline)
+
+# See build/core/soong_config.mk for the add_json_* functions you can use here.
+
+
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)