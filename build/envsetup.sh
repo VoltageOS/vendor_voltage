8417d0e53366725989bff217dab513510993d628
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-03 12:46:37 +0000

    envsetup: Cleanup
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,61 +1,23 @@
 function __print_voltage_functions_help() {
 cat <<EOF
 Additional functions:
-- mka:             Builds using SCHED_BATCH on all processors.
+- mka:             Builds using all available CPUs
+- brunch:          Lunch + mka in one command
 EOF
 }
 
 # Make using all available CPUs
 function mka() {
-    case `uname -s` in
-        Darwin)
-            make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-            ;;
-        *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
-            ;;
-    esac
+    m -j $(nproc --all) "$@"
 }
-
-function breakfast()
-{
-    target=$1
-    VOLTAGE_DEVICES_ONLY="true"
-    unset LUNCH_MENU_CHOICES
-    add_lunch_combo full-eng
-    for f in `/bin/ls vendor/voltage/vendorsetup.sh 2> /dev/null`
-        do
-            echo "including $f"
-            . $f
-        done
-    unset f
-
-    if [ $# -eq 0 ]; then
-        # No arguments, so let's have the full menu
-        echo "Nothing to eat for breakfast?"
-        lunch
-    else
-        echo "z$target" | grep -q "-"
-        if [ $? -eq 0 ]; then
-            # A buildtype was specified, assume a full device name
-            lunch $target
-        else
-            # This is probably just the voltage model name
-            lunch voltage_$target-userdebug
-        fi
-    fi
-    return $?
-}
-
-alias bib=breakfast
 
 function brunch()
 {
-    breakfast $*
+    lunch voltage_$1-userdebug
     if [ $? -eq 0 ]; then
         time mka bacon
     else
-        echo "No such item in brunch menu. Try 'breakfast'"
+        echo "Lunch failed!"
         return 1
     fi
     return $?
