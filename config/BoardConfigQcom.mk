d99b80221c459ce2d45710e09e6a8464ca7b51e0
SagarMakhar <sagarmakhar@gmail.com>
2021-09-18 06:20:37 +0000

    Add qti system/bt/conf to soong namespaces by default
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -151,7 +151,6 @@ ifneq ($(USE_DEVICE_SPECIFIC_DATASERVICES),true)
 endif
 
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
-    vendor/qcom/opensource/commonsys/system/bt/conf
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
 endif #TARGET_USE_QTI_BT_STACK
+PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/system/bt/conf
