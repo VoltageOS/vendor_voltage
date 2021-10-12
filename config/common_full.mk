49f1cf120c7e80f7f12796c68b3c308ab6e60356
Dmitrii <bankersenator@gmail.com>
2021-10-12 07:51:26 +0000

    vendor: drop los fonts stuff
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common_full.mk
+++ config/common_full.mk
@@ -3,16 +3,6 @@ $(call inherit-product, vendor/voltage/config/common_mobile.mk)
 
 PRODUCT_SIZE := full
 
-# Include {Lato,Rubik} fonts
-$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
-$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
-
-# Fonts
-PRODUCT_PACKAGES += \
-    fonts_customization.xml \
-    VoltageLatoFont \
-    VoltageRubikFont
-
 # Recorder
 PRODUCT_PACKAGES += \
     Recorder
