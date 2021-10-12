a0666281a4c38fac048bc7883f91348bf9e312ea
AshutoshSundresh <ashutoshsundresh@gmail.com>
2021-10-12 19:09:29 +0530

    vendor: Add build finished info
    Change-Id: I6b7a45705bb20280ed71a8226fa0d57530c28747
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -20,8 +20,18 @@ VOLTAGE_TARGET_PACKAGE := $(PRODUCT_OUT)/voltage-$(VOLTAGE_VERSION).zip
 
 SHA256 := prebuilts/build-tools/path/$(HOST_PREBUILT_TAG)/sha256sum
 
+CL_PRP="\033[35m"
+CL_RED="\033[31m"
+CL_GRN="\033[32m"
+
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
 	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(VOLTAGE_TARGET_PACKAGE)
 	$(hide) $(SHA256) $(VOLTAGE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(VOLTAGE_TARGET_PACKAGE).sha256sum
-	@echo "Package Complete: $(VOLTAGE_TARGET_PACKAGE)" >&2
+	echo -e ${CL_BLD}${CL_RED}"===============================-Package complete-==============================="${CL_RED}
+	echo -e ${CL_BLD}${CL_GRN}"Zip: "${CL_RED} $(VOLTAGE_TARGET_PACKAGE)${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"SHA256: "${CL_RED}" `cat $(VOLTAGE_TARGET_PACKAGE).sha256sum | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Size:"${CL_RED}" `du -sh $(VOLTAGE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"TimeStamp:"${CL_RED}" `cat $(PRODUCT_OUT)/system/build.prop | grep ro.voltage.build.date | cut -d'=' -f2 | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_GRN}"Integer Value:"${CL_RED}" `wc -c $(VOLTAGE_TARGET_PACKAGE) | awk '{print $$1}' `"${CL_RST}
+	echo -e ${CL_BLD}${CL_RED}"================================================================================"${CL_RED}
