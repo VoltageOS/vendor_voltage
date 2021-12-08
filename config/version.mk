ced770dc82fa1a0b92af479e894a20cbe714dfd5
Dmitrii <bankersenator@gmail.com>
2021-12-08 17:12:07 +0000

    vendor: VoltageOS 1.2
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 12.0
-VOLTAGEVERSION := 1.1
+VOLTAGEVERSION := 1.2
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
