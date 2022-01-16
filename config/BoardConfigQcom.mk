1e78e7b67076b344c07daffe8b556bb9639d2770
Pig <23kongw@brrice.edu>
2022-01-15 20:15:44 -0500

    voltage: Add vendor/qcom/opensource/commonsys/system/bt/main to soong ns
    Add one more product soong namespace to reflect S(QSSI 12) changes
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I84580b04fd34bad35435753f917694a4dd1cc3b9

--- config/BoardConfigQcom.mk
+++ config/BoardConfigQcom.mk
@@ -158,6 +158,8 @@ ifneq ($(USE_DEVICE_SPECIFIC_DATASERVICES),true)
 endif
 
 ifeq ($(TARGET_USE_QTI_BT_STACK),true)
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/packages/apps/Bluetooth
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf \
+    vendor/qcom/opensource/commonsys/system/bt/main
 endif #TARGET_USE_QTI_BT_STACK
-PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/commonsys/system/bt/conf
