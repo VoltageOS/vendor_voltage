78b83b7d65384fd3caa51c12b8bc9b789445e51c
Rajeev <33623043+RajeevNomad@users.noreply.github.com>
2022-02-06 13:42:13 +0000

    vendor: add 48 fps 720p bootanimation
    some devices struggle to render 60 fps at boot
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -4,6 +4,8 @@ ifeq ($(TARGET_BOOT_ANIMATION_RES),1280)
      PRODUCT_COPY_FILES += vendor/voltage/bootanimation/1280.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1920)
      PRODUCT_COPY_FILES += vendor/voltage/bootanimation/1920.zip:system/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1280-48)
+     PRODUCT_COPY_FILES += vendor/voltage/bootanimation/1280-48.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),2160)
      PRODUCT_COPY_FILES += vendor/voltage/bootanimation/2160.zip:system/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),2560)
