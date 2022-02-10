e736adab310d14443c4a66bc6f71fb222342bfaa
jhenrique09 <jhsv09@gmail.com>
2022-02-10 11:25:28 -0300

    vendor: Restore FaceUnlock
    Change-Id: I0beb2311a7fa469182b71c54f0773aac36b53354
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -124,6 +124,17 @@ PRODUCT_PACKAGES += \
     GrapheneOS-Camera
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= true
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face_unlock_service.enabled=$(TARGET_FACE_UNLOCK_SUPPORTED)
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Themes
 PRODUCT_PACKAGES += \
     AndroidBlackThemeOverlay \
