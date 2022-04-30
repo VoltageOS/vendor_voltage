2c745ca87ff34a9c153ce710707cf408dae2b45d
Dmitrii <bankersenator@gmail.com>
2022-04-30 10:59:15 +0300

    vendor: time for voltage 1.5

--- config/version.mk
+++ config/version.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ANDROID_VERSION := 12.0
-VOLTAGEVERSION := 1.4
+ANDROID_VERSION := 12.1
+VOLTAGEVERSION := 1.5
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
