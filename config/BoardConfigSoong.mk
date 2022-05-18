a8e4d7005ca64be234d298ce63a4fd6a1838be09
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-18 09:51:13 +0200

    soong: Drop TARGET_USES_QTI_CAMERA_DEVICE
    * qcom-camera topic hasn't been ported to 19.1
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ifb053d215527d602109bbb451d96ec80cf028548

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -55,8 +55,7 @@ SOONG_CONFIG_voltageQcomVars += \
     supports_hw_fde \
     supports_hw_fde_perf \
     uses_pre_uplink_features_netmgrd \
-    uses_qcom_bsp_legacy \
-    uses_qti_camera_device
+    uses_qcom_bsp_legacy
 
 # Only create display_headers_namespace var if dealing with UM platforms to avoid breaking build for all other platforms
 ifneq ($(filter $(UM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
@@ -80,7 +79,6 @@ SOONG_CONFIG_voltageQcomVars_supports_hw_fde := $(TARGET_HW_DISK_ENCRYPTION)
 SOONG_CONFIG_voltageQcomVars_supports_hw_fde_perf := $(TARGET_HW_DISK_ENCRYPTION_PERF)
 SOONG_CONFIG_voltageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_voltageQcomVars_uses_qcom_bsp_legacy := $(TARGET_USES_QCOM_BSP_LEGACY)
-SOONG_CONFIG_voltageQcomVars_uses_qti_camera_device := $(TARGET_USES_QTI_CAMERA_DEVICE)
 
 # Set default values
 BOOTLOADER_MESSAGE_OFFSET ?= 0
