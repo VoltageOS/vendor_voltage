26a1a26b5d659c232fb6acb1d02a42974349c91c
Dmitrii <bankersenator@gmail.com>
2022-05-21 13:21:13 +0000

    vendor: plan up 1.6 release
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 12.1
-VOLTAGEVERSION := 1.5
+VOLTAGEVERSION := 1.6
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
