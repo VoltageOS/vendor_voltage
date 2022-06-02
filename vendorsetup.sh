a996de115e0a67447589b17679e1f1212c8ca261
Dmitrii <bankersenator@gmail.com>
2022-06-02 02:15:09 +0000

    Revert "vendor: enable GlobalThinLO & ThinLTO chache"
    This reverts commit f361aa66f3e2c7fa02c8e44170adb9a45f6a404f.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,2 @@
 # For now, just skip the ABI checks to fix build errors.
 export SKIP_ABI_CHECKS=true
-
-# Enable ThinLTO Source wide.
-echo "Building with ThinLTO."
-export GLOBAL_THINLTO=true
-export USE_THINLTO_CACHE=true
