11be83821b1936d5d9419112be053049e19ef4b8
Michael Bestas <mkbestas@lineageos.org>
2022-08-30 03:08:53 +0300

    config: Remove BOOTLOADER_MESSAGE_OFFSET
    This hasn't been ported after lineage-18.1
    
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: Ie3d798a009a4e0042b767839b39dd6f68695ef2a

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -29,7 +29,6 @@ $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 SOONG_CONFIG_NAMESPACES += voltageGlobalVars
 SOONG_CONFIG_voltageGlobalVars += \
     additional_gralloc_10_usage_bits \
-    bootloader_message_offset \
     needs_camera_boottime \
     target_alternative_futex_waiters \
     target_init_vendor_lib \
@@ -59,14 +58,12 @@ SOONG_CONFIG_voltageQcomVars_supports_extended_compress_format := $(AUDIO_FEATUR
 SOONG_CONFIG_voltageQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 
 # Set default values
-BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_voltageGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
-SOONG_CONFIG_voltageGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_voltageGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_voltageGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_voltageGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
