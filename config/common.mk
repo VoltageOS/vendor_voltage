94cad60c9638f5f62562552f11a57ba4d5bd4315
Aaron Kling <webgeek1234@gmail.com>
2022-09-13 20:07:20 -0500

    config: Exclude product_manifest.xml from enforced existing packages
    Not all build targets have this target modules
    
    Change-Id: I01051bb0f6e285ca64bfbfbbaa70bfab796533eb
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -103,7 +103,7 @@ PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Require all requested packages to exist
-$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(VOLTAGE_BUILD)/$(TARGET_PRODUCT).mk),)
+$(call enforce-product-packages-exist-internal,$(wildcard device/*/$(LINEAGE_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml)
 
 # Config
 PRODUCT_PACKAGES += \
