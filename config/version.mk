132fbfdc03ceb33d62eb5b22285bd250530ba323
Dmitrii <bankersenator@gmail.com>
2022-12-03 13:34:12 +0000

    vendor: bump voltage version
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 13
-VOLTAGEVERSION := 2.0
+VOLTAGEVERSION := 2.1
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
