f73f155930d4f46930e31e4ebd4ff3fd0a9f4891
Dmitrii <bankersenator@gmail.com>
2023-01-30 03:42:38 +0000

    vendor: version 2.3
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 13
-VOLTAGEVERSION := 2.2
+VOLTAGEVERSION := 2.3
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
