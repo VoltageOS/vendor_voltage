36de95c2062a25a985594e502142f7d267b87077
Mathieu Chartier <mathieuc@google.com>
2023-03-05 16:15:50 +0530

    voltage: Speed profile services and wifi-service to reduce RAM and storage

--- config/common.mk
+++ config/common.mk
@@ -91,6 +91,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=log
 
+# Speed profile services and wifi-service to reduce RAM and storage
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
