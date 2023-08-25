5e9b8c46c3c74c01b69ff5ff58e0423ee352cfcb
Dmitrii <bankersenator@gmail.com>
2023-08-25 06:40:40 +0000

    vendor: set version to 2.8-EOL
    2.8 is our last a13 update
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 13
-VOLTAGEVERSION := 2.7
+VOLTAGEVERSION := 2.8-EOL
 
 VOLTAGE_BUILD_TYPE ?= UNOFFICIAL
 VOLTAGE_DATE_YEAR := $(shell date -u +%Y)
