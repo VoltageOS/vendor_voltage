92787eaae7f182eb8bb9403d256e54cd877f05b5
Bruno Martins <bgcngm@gmail.com>
2023-10-25 00:53:43 +0100

    vendor: voltage: Move all QCOM board configs to hardware/qcom-caf/common
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -7,6 +7,3 @@ endef
 
 # Include board/platform macros
 include vendor/voltage/build/core/utils.mk
-
-# Include vendor platform definitions
-include vendor/voltage/build/core/vendor/*.mk
--- config/BoardConfigVoltage.mk
+++ config/BoardConfigVoltage.mk
@@ -1,7 +1,7 @@
 include vendor/voltage/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/voltage/config/BoardConfigQcom.mk
+include hardware/qcom-caf/common/BoardConfigQcom.mk
 endif
 
 include vendor/voltage/config/BoardConfigSoong.mk
